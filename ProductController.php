66fbcdeeddf18806f32cde83df2073c0020841f1
Quentin <quentinsacla@gmail.com>
2024-04-11 11:51:30 +0100

    commit exo4

--- ProductController.php
+++ ProductController.php
@@ -1,27 +1,37 @@
 <?php
-class ProductController
-{
-    public function index()
-    {
-        $products = [];
+// ProductController.php
+class ProductController {
+    public function index() {
+        $products = []; 
+
         header('Content-Type: application/json');
         echo json_encode($products);
     }
 
-    public function new ()
-    {
+    public function new() {
+        // Afficher le formulaire pour ajouter un nouveau produit
         include_once __DIR__ . '/../templates/product/new.html';
+    }
+
+    public function edit($id) {
+        $product = new Product("Nom du produit", 10.99, new DateTime());
+        include_once __DIR__ . '/../templates/product/edit.html';
     }
 }
 
 $controller = new ProductController();
 
+// Vérification de l'action demandée
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $controller->new();
+    $id = $_GET['id'];
+    $controller->edit($id);
 } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST['id'];
     $name = $_POST['name'];
     $priceHt = $_POST['priceHt'];
-    $product = new Product($name, $priceHt, new DateTime());
+    $dateUpdate = new DateTime();
     header('Location: /product');
     exit;
 }
+
+
