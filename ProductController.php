afd5f3c76b4c0fdd086a3ed806ddf3993099d8f4
Quentin <quentinsacla@gmail.com>
2024-04-11 11:36:29 +0100

    commit template

--- ProductController.php
+++ ProductController.php
@@ -1,16 +1,27 @@
 <?php
-
-// ProductController.php
-class ProductController {
-    public function index() {
-        $products = []; 
+class ProductController
+{
+    public function index()
+    {
+        $products = [];
         header('Content-Type: application/json');
         echo json_encode($products);
+    }
+
+    public function new ()
+    {
+        include_once __DIR__ . '/../templates/product/new.html';
     }
 }
 
 $controller = new ProductController();
-$controller->index();
 
-
-?>
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $controller->new();
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $priceHt = $_POST['priceHt'];
+    $product = new Product($name, $priceHt, new DateTime());
+    header('Location: /product');
+    exit;
+}
